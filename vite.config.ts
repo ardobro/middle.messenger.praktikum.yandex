528a76da8914fe65f43df1032dcf12b85ff9679d
Artyom Dobrodomov <a.dobrodomov@dodopizza.com>
2023-10-18 23:34:24 +0300

    change port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import handlebars from "./vite-plugin-handlebars-precompile";
 import viteSvgLoader from "vite-svg-loader";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   css: {
     preprocessorOptions: {
       scss: {
